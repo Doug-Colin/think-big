bb3f109101ebcb3fedc9c71b91a28134667522d1
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-06-28 20:21:22 -0400

    feat(ui): :sparkles: complete mantine setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,26 @@
 import { SessionProvider } from "next-auth/react";
+import Head from 'next/head';
 import { AppProps } from "next/app";
+import { MantineProvider } from '@mantine/core';
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = (props: AppProps) => {
+  const { Component, pageProps } = props;
   return (
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      <Head>
+        <title>Page title</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
     </SessionProvider>
   );
 };
