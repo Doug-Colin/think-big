9ebcdfa9003176774fe5c48af6baeb3171568eae
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-06-30 14:11:40 -0400

    chore(style): :hammer: tabs for a11y

--- next.config.js
+++ next.config.js
@@ -2,23 +2,21 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    /* config options here */
+	/* config options here */
 }
 
-const withMDX = require( '@next/mdx' )( {
-    extension: /\.mdx?$/,
-    options: {
-        remarkPlugins: [],
-        rehypePlugins: [],
-        // If you use `MDXProvider`, uncomment the following line.
-        providerImportSource: "@mdx-js/react",
-    },
-} )
-module.exports = withMDX( {
-    // Append the default value with md extensions
-    pageExtensions: [ 'ts', 'tsx', 'js', 'jsx', 'md', 'mdx' ],
-} )
-
-
+const withMDX = require('@next/mdx')({
+	extension: /\.mdx?$/,
+	options: {
+		remarkPlugins: [],
+		rehypePlugins: [],
+		// If you use `MDXProvider`, uncomment the following line.
+		providerImportSource: '@mdx-js/react',
+	},
+})
+module.exports = withMDX({
+	// Append the default value with md extensions
+	pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+})
 
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,28 +4,28 @@ import { AppProps } from 'next/app'
 import { MantineProvider } from '@mantine/core'
 
 const App = (props: AppProps) => {
-    const { Component, pageProps } = props
-    return (
-        <SessionProvider session={pageProps.session}>
-            <Head>
-                <title>Page title</title>
-                <meta
-                    name="viewport"
-                    content="minimum-scale=1, initial-scale=1, width=device-width"
-                />
-            </Head>
-            <MantineProvider
-                withGlobalStyles
-                withNormalizeCSS
-                theme={{
-                    /** Put your mantine theme override here */
-                    colorScheme: 'light',
-                }}
-            >
-                <Component {...pageProps} />
-            </MantineProvider>
-        </SessionProvider>
-    )
+	const { Component, pageProps } = props
+	return (
+		<SessionProvider session={pageProps.session}>
+			<Head>
+				<title>Page title</title>
+				<meta
+					name='viewport'
+					content='minimum-scale=1, initial-scale=1, width=device-width'
+				/>
+			</Head>
+			<MantineProvider
+				withGlobalStyles
+				withNormalizeCSS
+				theme={{
+					/** Put your mantine theme override here */
+					colorScheme: 'light',
+				}}
+			>
+				<Component {...pageProps} />
+			</MantineProvider>
+		</SessionProvider>
+	)
 }
 
 export default App
