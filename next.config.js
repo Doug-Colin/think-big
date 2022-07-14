c5dbf5811825a3e39d9e70b7d80a9b8a62ebf694
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-07-14 15:39:45 -0400

    chore(conf): :hammer: tweaks

--- next.config.js
+++ next.config.js
@@ -1,22 +1,23 @@
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-	/* config options here */
+const { env } = require('./envcheck')
+
+function getConfig(config) {
+	return config
 }
 
-const withMDX = require('@next/mdx')({
-	extension: /\.mdx?$/,
-	options: {
-		remarkPlugins: [],
-		rehypePlugins: [],
-		// If you use `MDXProvider`, uncomment the following line.
-		providerImportSource: '@mdx-js/react',
-	},
-})
-module.exports = withMDX({
-	// Append the default value with md extensions
-	pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-})
+const nextConfig = {
+	reactStrictMode: true,
+}
 
-module.exports = nextConfig
+module.exports = getConfig({
+	/**
+	 * Dynamic configuration available for the browser and server.
+	 * @link https://nextjs.org/docs/api-reference/next.config.js/runtime-configuration
+	 */
+	publicRuntimeConfig: {
+		NODE_ENV: env.NODE_ENV,
+	},
+	nextConfig,
+})
