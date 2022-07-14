0bdfb723a00dac0069b3eb25e30a09b63cc83e78
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-07-14 19:12:46 -0400

    chore(app): :hammer: clean up imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import type { GetServerSideProps } from 'next'
-import Layout from 'components/Layout'
-import Post, { PostProps } from 'components/Post'
-import prisma from 'lib/prisma'
+
+import prisma from '~/lib/prisma'
 
 const Index = (props) => {
 	return <>Hello! I am a placeholder for something much better to come.</>
