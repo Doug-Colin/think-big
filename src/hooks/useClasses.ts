292fc4c964c8f96bc6c076244f0e7330c9cc1c3c
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-07-26 13:54:19 -0400

    fix(api): :bug: useClasses & fetchClasses for react-query

--- src/hooks/useClasses.ts
+++ src/hooks/useClasses.ts
@@ -1,18 +1,56 @@
-// import prisma from '~/lib/prisma'
-import { axiosClient } from '~/lib'
-const client = axiosClient()
-// import axios from 'axios'
+import { prisma, axiosClient } from '~/lib'
+
+import { useQuery } from '@tanstack/react-query'
 
 /**
- * It makes a GET request to the server, and returns the data that the server sends back
- * @returns An array of objects.
+ * It fetches all the classes from the database and returns them as a JSON object
+ * @returns An array of objects with the following properties:
+ * 	id: true,
+ * 	title: true,
+ * 	classNum: true,
+ * 	date: true,
+ * 	description: true,
+ * 	materialLinks: true,
+ * 	tags: true,
  */
-export const fetchClasses = async () => {
+const fetchClasses = async () => {
 	try {
-		const { data } = await client.get('/api/class/all')
+		// if (useAPI) {
+		// const client = axiosClient()
+		// const { data } = await client.get('/api/class/all')
+		// 	return data
+		// }
+		const classes = await prisma.class.findMany({
+			select: {
+				id: true,
+				title: true,
+				classNum: true,
+				date: true,
+				description: true,
+				materialLinks: true,
+				tags: true,
+			},
+		})
+		// const { json } = superjson.serialize(classes)
 
-		return data
+		return classes
 	} catch (err) {
 		console.error(err)
 	}
 }
+
+const fetchClassesAPI = async () => {
+	const fetch = axiosClient()
+	const { data } = await fetch.get('/api/class/all')
+	return data
+}
+
+const useClasses = () => {
+	console.log('useClasses called')
+	const result = useQuery(['classes'], fetchClassesAPI, {
+		staleTime: 1000 * 60 * 5,
+	})
+	return result
+}
+
+export { fetchClasses, fetchClassesAPI, useClasses }
