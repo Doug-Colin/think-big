301d477ae82c9ac5fa74a1a50cc29c047ed4d5d9
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-07-28 17:26:01 -0400

    feat(ui): :sparkles: pass className & tagSize props
    Co-authored-by: Jeannie Chen <jcvdg@users.noreply.github.com>

--- src/components/TagGroup.tsx
+++ src/components/TagGroup.tsx
@@ -7,14 +7,21 @@ interface TagGroupProps {
 		color: string
 	}[]
 	noWrap?: boolean
+	className?: any
+	tagSize?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
 }
 
-export const TagGroup = ({ tags, noWrap = true }: TagGroupProps) => {
+export const TagGroup = ({
+	tags,
+	noWrap = true,
+	className,
+	tagSize = 'sm',
+}: TagGroupProps) => {
 	const tagItems = tags.map((tag) => {
 		return (
 			<Badge
 				key={tag.tag}
-				size='sm'
+				size={tagSize}
 				styles={(theme) => ({
 					root: {
 						backgroundColor: tag.color,
@@ -27,7 +34,7 @@ export const TagGroup = ({ tags, noWrap = true }: TagGroupProps) => {
 	})
 
 	return (
-		<Group spacing={5} noWrap={noWrap}>
+		<Group spacing={5} noWrap={noWrap} className={className}>
 			{tagItems}
 		</Group>
 	)
