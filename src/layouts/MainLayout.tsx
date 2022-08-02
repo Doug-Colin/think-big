dbe9359c090579e33cb3eb03afebf575fe7493cf
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-08-02 12:02:26 -0400

    fix(ui): :bug: hide sidebar if unauthd

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,12 +1,21 @@
-import { AppShell } from '@mantine/core'
+import { AppShell, Center, Loader } from '@mantine/core'
 import { SideNav, HeaderBar } from '~/components'
+import { useSession } from 'next-auth/react'
 
 export const MainLayout = ({ children }) => {
+	const { data: session, status } = useSession()
+	if (status === 'loading') {
+		return (
+			<Center style={{ width: '100vw', height: '100vh' }}>
+				<Loader size='xl' />
+			</Center>
+		)
+	}
 	return (
 		<AppShell
 			padding='md'
 			fixed={false}
-			navbar={<SideNav />}
+			navbar={session && <SideNav />}
 			header={<HeaderBar />}
 		>
 			{children}
