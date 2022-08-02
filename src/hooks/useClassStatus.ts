c56ef3de197a0c1a2e8e725edc65003af16e6aa8
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-08-01 20:02:47 -0400

    feat(api,db): :sparkles: class status db update api/prisma function

--- src/hooks/useClassStatus.ts
+++ src/hooks/useClassStatus.ts
@@ -1,5 +1,5 @@
 import { prisma, axiosClient } from '~/lib'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import { userClassStatus } from '~/common/dbSelect'
 
 export interface UserClassStatusRecord {
@@ -14,45 +14,47 @@ export const keyClassStatuses = (userId: string) => [
 ]
 
 /**
- * It fetches all the classes from the database and returns them
+ * It fetches the class statuses of a user
+ * @param {string} userId - The user's id
  * @returns An array of objects with the following properties:
- * 	id: true,
- * 	title: true,
- * 	classNum: true,
- * 	date: true,
- * 	description: true,
- * 	materialLinks: true,
- * 	tags: true,
+ * 	- id
+ * 	- classId
+ * 	- classStatus
+ *
  */
 export const fetchClassStatuses = async (userId: string) => {
 	try {
-		const classStatuses = await prisma.user.findFirstOrThrow({
+		const data = await prisma.classStatus.findMany({
 			where: {
-				id: userId,
+				userId: { equals: userId },
 			},
-			select: {
-				id: true,
-				classes: {
-					select: userClassStatus,
-				},
-			},
+			select: userClassStatus,
 		})
-		// const { json } = superjson.serialize(classes)
-
-		return classStatuses
+		const formattedData = {
+			id: userId,
+			classes: data.map((result) => ({
+				id: result.classId,
+				status: result.status,
+			})),
+		}
+		return formattedData
 	} catch (err) {
 		console.error(err)
 	}
 }
 
 /**
- * It fetches all the classes from the server and returns them as an array of ClassRecord objects
- * @returns An array of ClassRecord objects
+ * It fetches the user's class status records from the server
+ * @param {string} userId - string - The user's ID
+ * @returns An object with the following properties:
+ * 	- userId: string
+ * 	- classId: string
+ * 	- status: string
  */
 const fetchAPI = async (userId: string): Promise<UserClassStatusRecord> => {
-	const fetch = axiosClient()
+	const client = axiosClient()
 	try {
-		const { data } = await fetch.get(`/api/user/${userId}/class/status/all`)
+		const { data } = await client.get(`/api/user/${userId}/class/status/all`)
 		return data
 	} catch (error) {
 		return error
@@ -60,11 +62,81 @@ const fetchAPI = async (userId: string): Promise<UserClassStatusRecord> => {
 }
 
 /**
- * `useClasses` is a React hook that returns the result of a query for all classes
+ * It updates the status of a class for a user
+ * @param {string} userId - The user's id
+ * @param {string} classId - The id of the class you want to update the status of
+ * @param newStatus - UserClassStatus['status']
+ */
+export const updateClassStatus = async (
+	userId: string,
+	classId: string,
+	newStatus: UserClassStatus['status']
+) => {
+	try {
+		const record = await prisma.classStatus.upsert({
+			where: {
+				classId_userId: {
+					userId,
+					classId,
+				},
+			},
+			update: {
+				status: newStatus,
+			},
+			create: {
+				userId,
+				classId,
+				status: newStatus,
+			},
+			select: userClassStatus,
+		})
+		const formattedData = {
+			id: record.userId,
+			status: [
+				{
+					classId: record.classId,
+					status: record.status,
+				},
+			],
+		}
+	} catch (error) {
+		console.error(error)
+	}
+}
+/**
+ * It updates the status of a class for a user
+ * @param {string} userId - the user's id
+ * @param {string} classId - the id of the class you want to update
+ * @param newStatus - UserClassStatus['status']
+ * @returns The data is being returned.
+ */
+export const updateClassStatusAPI = async (
+	userId: string,
+	classId: string,
+	newStatus: UserClassStatus['status']
+) => {
+	const client = axiosClient()
+	try {
+		const { data } = await client.put(
+			`/api/user/${userId}/class/update/${classId}`,
+			{ newStatus }
+		)
+		return data
+	} catch (err) {
+		console.error(err)
+	}
+}
+
+/**
+ * It's a function that takes a queryKey and a userId and returns the result of a query that uses the
+ * queryKey to fetch data from the API using the userId
+ * @param {string[]} queryKey - This is an array of strings that uniquely identify the query.
+ * @param {string} userId - The userId of the user you want to get the class statuses for.
  * @returns The result of the useQuery hook.
  */
 export const useClassStatuses = (queryKey: string[], userId: string) => {
 	console.log('useClassStatuses called')
 	const result = useQuery(queryKey, () => fetchAPI(userId))
+	console.log('useClassStatuses result', result)
 	return result
 }
--- src/pages/api/user/[userId]/class/[action]/[classId].ts
+++ src/pages/api/user/[userId]/class/[action]/[classId].ts
@@ -2,53 +2,51 @@ import { prisma } from '~/lib'
 import { getServerSession } from '~/pages/api/auth/[...nextauth]'
 import type { NextApiHandler } from 'next'
 import { userClassComposite } from '~/common/dbSelect'
-/**
- * It gets the session from the request, and if there is no session, it returns a 401 error. If there
- * is a session, it gets all the classes from the database and returns them
- * @param req - The request object.
- * @param res - the response object
- */
+import { fetchClassStatuses, updateClassStatus } from '~/hooks'
+import { z } from 'zod'
 
 interface QueryParams {
 	classId: string
 	userId: string
 	action: 'status' | 'update'
 }
+const updateClassSchema = z.object({
+	newStatus: z.enum(['not_started', 'in_progress', 'done']),
+})
+
 const handler: NextApiHandler = async (req, res) => {
 	const session = await getServerSession(req, res)
 	const { classId, userId, action } = req.query as Partial<QueryParams>
 	const { method, headers } = req
 
-	console.log(headers)
+	console.log(req.body)
 	if (!session) {
 		// res.status(401) // disabled for now during dev.
 		// res.end()
 		console.log('unauthed req for /api/classes/all')
 	}
-	switch (action) {
-		case 'status':
-			if (method !== 'GET') res.status(400).end()
-			try {
-				const classes = await prisma.user.findFirstOrThrow({
-					where: {
-						id: userId,
-						classes: {
-							some: {
-								classId: classId,
-							},
-						},
-					},
-
-					select: {
-						id: true,
-						classes: {
-							select: userClassComposite,
-						},
-					},
-				})
+	try {
+		switch (action) {
+			case 'status':
+				if (method !== 'GET') res.status(400).end()
+				const classes = await fetchClassStatuses(userId)
 				res.status(200).json(classes)
-			} catch (error) {
-				res.status(500).json(error)
-			}
+				break
+			case 'update':
+				if (method !== 'PUT') res.status(400).end()
+				const reqBody = updateClassSchema.parse(req.body)
+				console.log(reqBody)
+				const statusUpdate = await updateClassStatus(
+					userId,
+					classId,
+					reqBody.newStatus
+				)
+				res.status(200).json(statusUpdate)
+				break
+		}
+	} catch (error) {
+		res.status(500).json(error)
 	}
 }
+
+export default handler
