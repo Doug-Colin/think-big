207eb2b0921fc9553fc63c05f451c58bf9e5fd72
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-08-03 11:30:36 -0400

    chore(conf): :hammer: config logging

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -15,7 +15,20 @@ if (typeof window === 'undefined') {
 		prisma = new PrismaClient()
 	} else {
 		if (!global.prisma) {
-			global.prisma = new PrismaClient()
+			global.prisma = new PrismaClient({
+				log: ['error', 'warn'],
+				errorFormat: 'pretty',
+			})
+			global.prisma.$use(async (params, next) => {
+				const before = Date.now()
+				const result = await next(params)
+				const after = Date.now()
+				console.info(
+					'\x1b[1mPrisma\x1b[0m',
+					`query ${params.model}.${params.action} took ${after - before}ms`
+				)
+				return result
+			})
 		}
 
 		prisma = global.prisma
