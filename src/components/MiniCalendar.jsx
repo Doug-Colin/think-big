da846bc1807f2e6131c66a3e0010608700cf3888
Douglas Colin Moore <dougcolin@gmail.com>
2022-09-03 18:02:15 -0400

    feat: ensured functionality of MiniCalendar.jsx after adding to src/components

--- src/components/MiniCalendar.jsx
+++ src/components/MiniCalendar.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import { Calendar } from '@mantine/dates'
 
-function MiniCalendar() {
+export const MiniCalendar = () => {
 	const [month, onMonthChange] = useState(new Date())
 
 	return <Calendar month={month} onMonthChange={onMonthChange} />
 }
+
+export default MiniCalendar
